420a55e81cffbfbb128704404ce7f0b4891da75e
Yukisilviani <92262087+Yukisilviani@users.noreply.github.com>
2023-05-27 08:58:58 +0700

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'flutter_flow/flutter_flow_theme.dart';
+import 'flutter_flow/flutter_flow_util.dart';
+import 'flutter_flow/internationalization.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'flutter_flow/nav/nav.dart';
+import 'index.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await FlutterFlowTheme.initialize();
+
+  runApp(MyApp());
+}
